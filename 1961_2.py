8697c32677febea8f30d043e63c1d08d65c2fa9c
정승원 <geobera0910@naver.com>
2019-07-31 17:59:57 +0900

    Update

--- 1961_2.py
+++ 1961_2.py
@@ -4,32 +4,17 @@ sys.stdin = open('1961.txt', 'r')
 for tc in range(int(input())):
     two = []
     r1, r2, r3 = ([], [], [])
-
     B = int(input())
     for b in range(B):
-        one_dimension_board = list(map(str, input().split()))
-        two.append(one_dimension_board)
-
+        two.append(list(map(str, input().split())))
     two = list(map(list, zip(*two)))
     for b1 in range(B):
-        two[b1][::-1]
-        frist_result = ''.join(two[b1][::-1])
-        r1.append(frist_result)
-
-    for b3 in range(B):
-        two[b3]
-        third_result = ''.join(two[b3])
-        r3.append(third_result)
-        
-    second_two = list(map(list, zip(*two)))
+        r1.append(''.join(two[b1][::-1]))
+        r3.append(''.join(two[b1]))
+    s_two = list(map(list, zip(*two)))
     for b2 in range(B):
-        second_two[b2][::-1]
-        second_result = ''.join(second_two[b2][::-1])
-        r2.append(second_result)
-
-    r2 = r2[::-1]
-    r3 = r3[::-1]
-    
+        r2.append(''.join(s_two[b2][::-1]))
+    r2, r3 = r2[::-1], r3[::-1]
     print('#{}'.format(tc+1, end=''))
     for b4 in range(B):
         print('{} {} {}'.format(r1[b4], r2[b4], r3[b4]))
